805e53d510e8a57174a760419aa293e2d4d22070
schoenentag <siny9713@gmail.com>
2022-01-14 23:41:00 +0900

    ByteVariable.java

---
+++ CharacterEX.java
@@ -0,0 +1,21 @@
+package chapter2;
+
+public class CharacterEX {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char ch = 'A';
+		System.out.println(ch);
+		System.out.println((int)ch);
+		
+		ch = 66;
+		System.out.println(ch);
+		
+		int ch2 = 67;
+		System.out.println(ch2);
+		System.out.println((char)ch2);
+				
+
+	}
+
+}
