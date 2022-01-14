3361638d2178ecfd5fcb35bbdf6c3f008c701ed2
schoenentag <siny9713@gmail.com>
2022-01-14 23:42:52 +0900

    Revert "Revert "ByteVariable.java""
    This reverts commit bae1c65b3dd7d1e91f8cf97f38b572a7aad1f97f.

---
+++ ByteVariable.java
@@ -0,0 +1,21 @@
+package chapter2;
+
+public class ByteVariable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte bData = -128;
+		System.out.println(bData);
+		
+		byte bData2 = 127;
+		System.out.println(bData2);
+		
+		//
+		
+		long num = 12345678900L;
+		System.out.println(num);
+		
+
+	}
+
+}
---
+++ VariableEX.java
@@ -0,0 +1,18 @@
+package chapter2;
+
+public class VariableEX {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num; // 정수 선언
+		num = 10; // 값 대입
+
+		System.out.println(num);
+		
+		int level = 20;
+		System.out.println(level);
+		
+		
+	}
+
+}
