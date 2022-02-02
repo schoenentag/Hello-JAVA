0eb41e221ce7c731da16da0b9c2499a79d1ff326
schoenentag <siny9713@gmail.com>
2022-02-02 23:09:29 +0900

    switch.java

---
+++ Switch.java
@@ -0,0 +1,37 @@
+package hausaufgaben;
+
+public class Switch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int day = 5;
+		switch(day) {
+		case 1:
+			System.out.println("Monday");
+			break;
+		case 2:
+			System.out.println("Tuesday");
+			break;
+		case 3:
+			System.out.println("Wednesday");
+			break;
+		case 4:
+			System.out.println("Thursday");
+			break;
+		case 5:
+			System.out.println("Friday");
+			break;
+		case 6:
+			System.out.println("Saturday");
+			break;
+		case 7:
+			System.out.println("sunday");
+			break;
+		default:
+		System.out.println("1~7사이의 숫자를 입력하십시오.");		
+		}
+				
+
+	}
+
+}
