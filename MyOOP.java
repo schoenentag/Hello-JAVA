eaccdd694d9197a89d658d7c43b0360143fe3590
schoenentag <siny9713@gmail.com>
2022-02-24 01:00:08 +0900

    InstanceReview_Start.java

--- MyOOP.java
+++ MyOOP.java
@@ -4,11 +4,28 @@ public class MyOOP {
 //public 접근 제어자 public은 한 번만 등장
 	public static void main(String[] args) {
 		
-		System.out.println("main 부분!!");
-		Print.delimiter = "----";
-		Print.A();
-		Print.B();
+		Print p1 = new Print();
+		p1.delimiter = "----";
+		/*인스턴스로 만들기 위해선 Print.java(class 생성 한 곳)에서
+		 * public static void A()였던 부분에서 static(class 소속)지워야함
+		 * public void A()
+		 */
+		p1.A();
+		p1.A();
+		p1.B();
+		p1.B();
 		
+		Print p2 = new Print();
+		p2.delimiter = "****";
+		p2.A();
+		p2.A();
+		p2.B();
+		p2.B();
+
+		p1.A();
+		p2.A();
+		p1.A();
+		p2.A();
 	}
 	}
 
--- Print.java
+++ Print.java
@@ -1,14 +1,14 @@
 package ex;
 
 class Print {
-	public static String delimiter = "";
+	public String delimiter = "";
 	
-	public static void A() {
+	public  void A() {
 	System.out.println(delimiter);
 	System.out.println("A");
 	System.out.println("A");
 	}
-	public static void B() {
+	public  void B() {
 	System.out.println(delimiter);
 	System.out.println("B");
 	System.out.println("B");
